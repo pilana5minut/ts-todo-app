b321f638fbedfa3dc494e64dcfe97d03fa26e3fa
Maxim Mogilevskiy <mogilevskiymv@gmail.com>
2025-08-07 15:59:05 +0300

    Обновляет стили всего приложения

--- src/features/todos/TodoList.tsx
+++ src/features/todos/TodoList.tsx
@@ -6,11 +6,17 @@ const TodoList = () => {
   const listTodo = useAppSelector((state) => state.todos.todos)
 
   return (
-    <ul className={cl.todoList}>
-      {listTodo.map((todo) => (
-        <TodoItem key={todo.id} {...todo} />
-      ))}
-    </ul>
+    <>
+      {listTodo.length ? (
+        <ul className={cl.todoList}>
+          {listTodo.map((todo) => (
+            <TodoItem key={todo.id} {...todo} />
+          ))}
+        </ul>
+      ) : (
+        <p className={cl.message}>No tasks were created</p>
+      )}
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.tsx'
+import './main.css'
 import { store } from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
